683453d1f452fd845867c1d6a592de28d3e0750b
Eyeless356 <95523501+Eyeless356@users.noreply.github.com>
2022-11-17 11:24:00 +0900

    deploy

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ root.render(
   <Provider store={store}>
     <PersistGate loading={3000} persistor={persistor}>
       <GlobalStyles />
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<Index />} />
